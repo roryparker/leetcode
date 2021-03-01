4cc21524f7323ea0498b7c2b8f1bf9178d0c57f2
roryparker <rorysethparker@protonmail.com>
2021-03-01 09:00:00 -0500

    new commit

--- add-binary/add-binary.js
+++ add-binary/add-binary.js
@@ -4,18 +4,19 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
-    
-    let result = "", carry = 0;
-    
-    while (a || b || carry) {
-        let sum = +a.slice(-1) + +b.slice(-1) + carry; // return the last element in array in numeric form
-        
-        if ( sum > 1) {
+
+    let result = "",
+        carryover = 0;
+
+    while (a || b || carryover) {
+        let sum = +a.slice(-1) + +b.slice(-1) + carryover; // return the last element in array in numeric form
+
+        if (sum > 1) {
             result = sum % 2 + result;
             carry = 1;
-        }else {
+        } else {
             result = sum + result;
-            carry = 0;
+            carryover = 0;
         }
         a = a.slice(0, -1)
         b = b.slice(0, -1)
@@ -23,7 +24,3 @@ var addBinary = function(a, b) {
 
     return result;
 };
-
-// x.split(" ").map(function(element) {
-//         return x.fromCharCode(parseInt(element, 2));
-//     }).join("")
