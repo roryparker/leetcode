8693de2452809aa9ca865be901be263db9e1bd31
roryparker <78453378+roryparker@users.noreply.github.com>
2021-06-28 20:43:58 -0400

    Time: 128 ms (99.31%), Space: 44.6 MB (77.94%) - LeetHub

---
+++ roman-to-integer/roman-to-integer.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    let x = {I:1, V:5, X:10, L:50, C:100, D:500, M:1000};
+    let total = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        if (x[s[i]] < x[s[i + 1]]) {
+            total = total + x[s[i + 1]] - x[s[i]];
+            i++;
+        } else { total = total + x[s[i]]};
+    }
+    
+    return total;
+};
