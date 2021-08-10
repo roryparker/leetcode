5d345051e41f57664142434d717bf2074cfd7a4d
roryparker <78453378+roryparker@users.noreply.github.com>
2021-08-09 20:01:19 -0400

    Time: 64 ms (96.18%), Space: 38.5 MB (90.60%) - LeetHub

--- length-of-last-word/length-of-last-word.js
+++ length-of-last-word/length-of-last-word.js
@@ -2,21 +2,10 @@
  * @param {string} s
  * @return {number}
  */
-// var lengthOfLastWord = function(s) {
-
-//     let length = 0
-    
-//     for (let i = 0; i <= s.length; i++) {
-//         if (s.charAt(i) == " " ) {
-//             break;
-//         } 
-//         else { 
-//             length++; 
-//     }
-//     return length;
-//    }
-// };
-
-var lengthOfLastWord = function(s) {    
-    return s.trimEnd().split(" ").pop().length  // - trims the array spaces, splits the string on the space, removes the word "World", and then counts the length.
+var lengthOfLastWord = function(s) {
+    return s
+            .trim()
+            .split(' ')
+            .slice(-1)
+            [0].length;
 };
