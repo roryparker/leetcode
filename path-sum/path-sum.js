16bb3c32c0bd472ea95d9305628bb9d484612907
roryparker <78453378+roryparker@users.noreply.github.com>
2021-09-20 20:46:59 -0400

    Time: 84 ms (70.53%), Space: 42.5 MB (76.28%) - LeetHub

---
+++ path-sum/path-sum.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+
+var hasPathSum = function(root, sum) {
+    if(!root) {
+        return false;
+    }
+    if(!root.left && !root.right) {
+        return root.val == sum ? true : false;
+    }
+    let remain = sum - root.val
+    return hasPathSum(root.left, remain) || hasPathSum(root.right, remain)
+};
