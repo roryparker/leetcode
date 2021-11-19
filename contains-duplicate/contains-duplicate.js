dfb9eae29db5af63c399fb4c76f354b9e17f353c
Rory Parker <78453378+roryparker@users.noreply.github.com>
2021-11-18 20:36:40 -0500

    Time: 84 ms (73.36%), Space: 47.2 MB (12.82%) - LeetHub

---
+++ contains-duplicate/contains-duplicate.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+var containsDuplicate = function(nums) {
+    let inStorage = {};
+    
+    for( let i=0 ; i < nums.length ; i++){
+        if(inStorage[nums[i]]) {
+            return true;
+        }else{
+            inStorage[nums[i]] = true
+        }
+    }
+    
+    return false
+    
+};
