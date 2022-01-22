3d40ebfcfbd3ab91fe740abc193894d5e8cc48d2
Rory Parker <78453378+roryparker@users.noreply.github.com>
2022-01-22 13:52:12 -0500

    Time: 5304 ms (5.13%), Space: 47.3 MB (60.03%) - LeetHub

---
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.js
@@ -0,0 +1,54 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+// var findDuplicates = function(nums) {
+//     let set = new Set(nums);
+    
+//     nums.forEach (i => {
+//         if (set.has(i)) {
+//             set.delete(i);
+//         } else {
+//             set.add(i);
+//         }
+//     })
+//     return Array.from(set);
+// };
+
+// var findDuplicates = function(nums) {
+//     let storage = new Map();
+    
+//     for (let i = 0; i < nums.length; i++) {
+//         if (storage.has(nums[i])) {
+//             storage.add(nums[i])
+//         }
+//     }return storage
+// };
+
+// var findDuplicates = function(nums) {
+//     let storage = [];
+    
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] - 1 > 0) nums[i] - 1 = -nums[i] - 1
+//         else { 
+//             storage.push(nums[i])
+//         }
+//     }
+    
+//     return storage;
+// };
+
+var findDuplicates = function(nums) {
+    let storage = [];
+    
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                storage.push(nums[i]);
+            }
+        }
+    }
+    
+    return storage;
+
+};
