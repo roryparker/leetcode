e4c5c09df1813f46fc0179621d4d9223ba83d75c
Rory Parker <78453378+roryparker@users.noreply.github.com>
2022-02-18 20:11:44 -0500

    Time: 112 ms (60.04%), Space: 44.5 MB (37.92%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+  const rowSet = new Set(), colSet = new Set()
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (!matrix[i][j]) {
+        rowSet.add(i);
+        colSet.add(j);
+      };
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (rowSet.has(i) || colSet.has(j)) matrix[i][j] = 0
+    }
+  }
+};
