104b749efff6b51973645b868082198398186a6d
Rory Parker <78453378+roryparker@users.noreply.github.com>
2022-02-24 20:37:13 -0500

    Time: 94 ms (53.67%), Space: 45.5 MB (5.53%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -3,6 +3,15 @@
  * @param {number} target
  * @return {number}
  */
+var search = function(nums, target) {
+    nums.forEach((element) => {
+        if (element == target) {
+            console.log(nums.indexOf(element))
+            return nums.indexOf(element);
+        }else return -1
+    })
+};
+
 var search = (nums, target) => {
     if (nums.map(x => x !== target).includes(false)) {
       target
