3db80f383f4b4bc3239a010ae61faa8f5daf94e9
Rory Parker <78453378+roryparker@users.noreply.github.com>
2022-03-26 12:29:17 -0400

    Time: 110 ms (38.82%), Space: 44.6 MB (87.03%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {    
+    while(head){
+        if(head.val){
+            head.val = false;
+            head = head.next;
+        }else{
+            return true;
+        }            
+    }
+    
+    return false;
+};
