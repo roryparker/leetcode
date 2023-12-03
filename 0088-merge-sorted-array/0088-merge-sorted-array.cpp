4147dbcf12e2dcd20d20ab093013f9f273d7e913
Rory Parker <78453378+roryparker@users.noreply.github.com>
2023-12-02 19:34:27 -0500

    Time: 2 ms (48.77%), Space: 9.4 MB (75.87%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+
+class Solution {
+public:
+    void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n) {
+        // Initialize pointers for nums1, nums2, and the end of merged array
+        int i = m - 1;  // Pointer for nums1
+        int j = n - 1;  // Pointer for nums2
+        int k = m + n - 1;  // Pointer for the end of merged array
+
+        // Merge nums1 and nums2 from the end
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        // If there are remaining elements in nums2, copy them to nums1
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+};
