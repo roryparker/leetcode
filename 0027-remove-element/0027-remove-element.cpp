3fd11ad6d966a2d30650f689e27e496e48daad68
Rory Parker <78453378+roryparker@users.noreply.github.com>
2023-12-21 22:56:31 -0500

    Time: 0 ms (100.00%), Space: 9.1 MB (38.60%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+    int k = 0; // Variable to track the number of elements not equal to val
+
+    // Iterate through the array using two pointers
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] != val) {
+            
+            // If the current element is not equal to val, move it to the front of the array
+            nums[k] = nums[i];
+            k++; // Increment the count of elements not equal to val
+        }
+    }
+        
+    // The first k elements now contain the elements not equal to val
+    // The remaining elements in the array are not important for the result
+
+    
+    return k; // Return the count of elements not equal to val
+    }
+};
